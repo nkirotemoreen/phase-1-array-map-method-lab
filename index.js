a39a283f6aafe9cfeaebd0c69b96df4fb3d40741
moreenisaac <nkirotemoreen70@gmail.com>
2022-08-25 00:49:39 +0300

    complete assignment

--- index.js
+++ index.js
@@ -10,14 +10,33 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+//check each word ,capitalize the first character of each word
+//loop through each sentence and check each word.
 
+//let us split the words
+//const str = 'Hello world';
+
+//const split = str.split(' ')
+//console.log(split) 
 function titleCased(){
-  const capTutorials = tutorials.map(t => {
-    const words = t.split(" ").map(w => {
-      return w.charAt(0).toUpperCase() + w.slice(1);
-    }).join(" ");
-    console.log(words);
-    return words; 
-  })
-  return capTutorials;
-}
+    const capTutorials = tutorials.map(t => {
+      const words = t.split(" ").map(w => {
+        return w.charAt(0).toUpperCase() + w.slice(1);
+      }).join(" ");
+      console.log(words);
+      return words; 
+    })
+    return capTutorials;
+  }
+
+  //I have a question on how to join the words in one sentence using the easier method
+
+  /*--const titleCased = tutorials.map(words => words.split(" ")//splits the strings to separate words
+.map(capsLetter => capsLetter.charAt(0) //checks the firstlettter of the words and capitalizes them
+  .toUpperCase() + capsLetter.slice(1))).join(" ");
+  console.log(titleCased)--*/
+
+
+
+  //console.log(tutorials.map(words => words.split(" ").map(capsLetter => capsLetter.charAt(0)
+  //.toUpperCase() + capsLetter.slice(1))).join(" "))
---
+++ test/try.js
@@ -0,0 +1,10 @@
+/*--const titleCased = tutorials.map(words => words.split(" ") //splits the strings to separate words
+.map(capsLetter => capsLetter.charAt(0) //checks the firstlettter of the words and capitalizes them
+  .toUpperCase() + capsLetter.slice(1))).join(" ");
+  console.log(titleCased)
+
+
+  
+  const titleCased = tutorials.map(words => words.split(" ").map(capsLetter => capsLetter.charAt(0)
+  .toUpperCase() + capsLetter.slice(1)));--*/
+  
